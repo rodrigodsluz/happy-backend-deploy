0f647f357e2784a0071da6305c1c992ce93bb519
rodrigodsluz <rodrigodsluz@gmail.com>
2020-10-28 15:53:47 -0300

    Configure deploy

---
+++ src/views/images_view.ts
@@ -0,0 +1,16 @@
+import Image from '../models/Image';
+
+export default {
+  render(image: Image) {
+    return {
+      id: image.id,
+      url: `${process.env.API_URL}/uploads/${image.path}`
+      /* url: `http://localhost:3333/uploads/${image.path}`, */
+      /* url: `http://192.168.0.108:3333/uploads/${image.path}`, */
+    };
+  },
+
+  renderMany(images: Image[]) {
+    return images.map((image) => this.render(image));
+  },
+};
